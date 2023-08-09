5c20cdb174873870dd200a284d949b7b21ddf768
Siigull <pelanekd@gmail.com>
2023-08-09 15:43:35 +0200

    changes in the register.html

--- Phordle/user/forms.py
+++ Phordle/user/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Enter email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Enter password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm password'}))
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
