d669a90d228b3a167ad9d0367e263a1d632e747b
Siigull <pelanekd@gmail.com>
2023-08-09 15:48:12 +0200

    messing with Image model

--- Phordle/feed/models.py
+++ Phordle/feed/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Image(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     object = models.CharField(max_length=50, default='', null=True)
     image = models.ImageField(upload_to = 'images/')
     likes = models.IntegerField(default=0, null=True)
