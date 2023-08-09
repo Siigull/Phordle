86c20bb16780ceea2f39a98a2b0ffce421af5c21
Siigull <pelanekd@gmail.com>
2023-08-09 15:36:39 +0200

    added navbar

--- Phordle/feed/urls.py
+++ Phordle/feed/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from . import views
 
 app_name = "feed"
-urlpatterns = [ path("", views.FeedView.as_view(), name="feed")]
+urlpatterns = [ path("", views.FeedView.as_view(), name='feed')]
