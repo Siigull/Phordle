dcecb752b715dbd3cc5b8aea0c2e590a836b5f0a
Siigull <pelanekd@gmail.com>
2023-08-10 10:50:34 +0200

    Improved register page

--- Phordle/user/forms.py
+++ Phordle/user/forms.py
@@ -4,12 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 class RegisterForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter username'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Enter email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Enter password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm password'}))
+    username = forms.CharField(label="Username:", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter username'}))
+    email = forms.EmailField(label="Email:", widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Enter email'}))
+    password1 = forms.CharField(label="Password:", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Enter password'}))
+    password2 = forms.CharField(label="Confirm Password:", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Confirm password'}))
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-        
+        fields = ('username', 'email', 'password1', 'password2')
